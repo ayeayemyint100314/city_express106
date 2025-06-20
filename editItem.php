5346139f21a366af7fe1562129a14a4342fdc5c0
ayeayemyint100314 <ayeayemyint0709@gmail.com>
2025-06-20 16:09:35 +0630

    20 june update item finished

--- editItem.php
+++ editItem.php
@@ -1,29 +1,60 @@
-
 <?php
 require_once "dbconn.php";
-if(!isset($_SESSION))
-{
+if (!isset($_SESSION)) {
     session_start();
 }
 
-    if(isset($_GET['id']))
-    {
-        $sql = "SELECT i.item_id, i.item_name, i.price,
-		    c.cname as category ,
+$sql = "select c.cid, c.cname from category c ";
+$stmt = $conn->query($sql);
+$stmt->execute();
+$categories = $stmt->fetchAll();
+
+if (isset($_GET['id'])) {
+    $sql = "SELECT i.item_id, i.item_name, i.price,
+		    c.cname as cname , c.cid as cid ,
 		    i.description, i.quantity,
 		    i.img_path
             from item i, category c
             where i.category = c.cid AND
             i.item_id = ? ";
 
-           $stmt =  $conn->prepare($sql);
-           $stmt->execute([$_GET['id']]);
-           $item = $stmt->fetch();
-           print_r($item);
+    $stmt =  $conn->prepare($sql);
+    $stmt->execute([$_GET['id']]);
+    $item = $stmt->fetch();
+}
 
+if (isset($_POST['updateItem'])) {
 
-
+    $name = $_POST['itemName'];
+    $price = $_POST['price'];
+    $description = $_POST['description'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
+    $category = $_POST['category'];
+    $filename =  $_FILES['img']['name'];
+    $filepath = "item_img/" . $filename;
+    $id = $_POST['id'];
+    $status = move_uploaded_file($_FILES['img']['tmp_name'],  $filepath);
+    if ($status) // storing file to a specified directory is sussessful
+    {
+        try {
+            $sql = "update item set item_name=?, price=?,
+                     description=?, quantity=?, category=?,
+                     img_path=? 
+                     where item_id=?";
+            $stmt = $conn->prepare($sql);
+            // null is for ID which is auto increment primary key
+            $status = $stmt->execute([ $name, $price,  $description, $quantity, $category,  $filepath, $id]);
+            $lastId = $id;
+            if ($status) {
+                $_SESSION['updateSuccess'] = "Item with $lastId has been updated successfully!";
+                header("Location:viewItem.php");
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
+}
 
 
 
@@ -33,14 +64,87 @@ if(!isset($_SESSION))
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
 </head>
-<body>
 
-<p></p>
-    
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <?php require_once "navbarbar.php";  ?>
+
+        </div>
+        <div class="row my-5">
+            <div class="col-md-4 mx-auto">
+                <h4 class="text-center">Update Item </h4>
+                <form class="border border-primary p-3" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+                    <input type="hidden" name="id" value="<?php echo $item['item_id'] ?>">
+                    <div class="mb-3">
+                        <label for="itemName" class="form-label">Name</label>
+                        <input type="text" class="form-control" name="itemName" value="<?php echo $item['item_name'] ?>">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="price" class="form-label"> Price</label>
+                        <input type="number" class="form-control" name="price" value="<?php echo $item['price'] ?>">
+
+                    </div>
+                    <div class="mb-3">
+                        <label for="quantity" class="form-label"> Quantity</label>
+                        <input type="number" class="form-control" name="quantity" value="<?php echo $item['quantity'] ?>">
+
+                    </div>
+                    <div class="mb-3">
+                        <img src="<?php echo $item['img_path'] ?>"" alt="">
+                        <label for=" formFileSm" class="form-label">item image</label>
+                        <input class="form-control form-control-sm" name="img" type="file">
+                    </div>
+                    <div class="mb-3">
+                        <p><?php echo $item['cname']; ?></p>
+                        <select class="form-select" name="category">
+                            <option selected>Open this select menu</option>
+                            <?php
+                            if (isset($categories)) {
+                                foreach ($categories as $category) {
+                            ?>
+                                    <option value="<?php echo $category['cid']; ?>">
+                                        <?php echo $category['cname']; ?></option>
+
+                            <?php }
+                            } ?>
+
+                        </select>
+
+                    </div>
+
+                    <div class="form-floating">
+                        <textarea class="form-control" value="<?php echo $item['description']; ?>" name="description" placeholder="write description here" id="floatingTextarea"></textarea>
+                        <label for="floatingTextarea">Description</label>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary" name="updateItem">Update Item</button>
+                </form>
+
+
+
+
+
+            </div>
+
+
+
+
+        </div>
+
+    </div>
+
+
+
 </body>
+
 </html>
--- viewItem.php
+++ viewItem.php
@@ -1,10 +1,9 @@
 <?php
 require_once "dbconn.php";
-if(!isset($_SESSION))
-{
+if (!isset($_SESSION)) {
     session_start(); // create a session if not exists
 }
-try{
+try {
     $sql = "SELECT i.item_id, i.item_name, i.price,
 		    c.cname as category ,
 		    i.description, i.quantity,
@@ -16,8 +15,7 @@ try{
     $stmt->execute();
     $items = $stmt->fetchAll(); //print_r($items);
 
-}catch(PDOException $e)
-{
+} catch (PDOException $e) {
     echo $e->getMessage();
 }
 
@@ -27,32 +25,37 @@ try{
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Item</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
 
 </head>
+
 <body class="bg-light">
     <div class="container-fluid">
-       <div class="row">
-        <?php require_once "navbarbar.php"; ?>
-       </div>
-            <div class="row">
-                
-                <div class="col-md-10 my-5 mx-auto">
-                    <?php 
-                    if(isset($_SESSION['insertSuccess']))
-                    {
+        <div class="row">
+            <?php require_once "navbarbar.php"; ?>
+        </div>
+        <div class="row">
+
+            <div class="col-md-10 my-5 mx-auto">
+                <?php
+                if (isset($_SESSION['insertSuccess'])) {
                     echo "<p class='alert alert-success'>$_SESSION[insertSuccess] </p>";
                     unset($_SESSION['insertSuccess']);
-                    }
-                    
-                    
-                    ?>
-                   <table class="table table-striped">
+                } else  if (isset($_SESSION['updateSuccess'])) {
+                    echo "<p class='alert alert-success'>$_SESSION[updateSuccess] </p>";
+                    unset($_SESSION['updateSuccess']);
+                }
+
+
+                ?>
+                <p class="text-start"><a class="btn btn-outline-primary mx-auto" href="./insert_item.php">Add New Item</a></p>
+                <table class="table table-striped">
                     <thead>
                         <tr>
                             <th>Item Name</th>
@@ -65,12 +68,10 @@ try{
 
                     </thead>
                     <tbody>
-                        <?php   
-                        if(isset($items))
-                        {
-                           foreach($items as $item)
-                           {
-                        echo "<tr>
+                        <?php
+                        if (isset($items)) {
+                            foreach ($items as $item) {
+                                echo "<tr>
                                 <td>$item[item_name]</td>
                                 <td>$item[price]</td>
                                 <td class=text-wrap>$item[description]</td>
@@ -79,16 +80,17 @@ try{
                                 <td><img style=width:60px;height:80px; src=$item[img_path]></td>  
                                 <td><a class='btn btn-primary rounded-pill' href=editItem.php?id=$item[item_id]>Edit</a></td>           
                               </tr>";
-                           }     
-                        }                        
+                            }
+                        }
                         ?>
                     </tbody>
-                   </table>
-                </div>
-
-
+                </table>
             </div>
+
+
+        </div>
     </div>
-    
+
 </body>
+
 </html>
